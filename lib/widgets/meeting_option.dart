d1d5735f72ca83f01673bdf546f99196d80b2498
mithoo18 <devanshu18g@gmail.com>
2022-11-14 18:22:16 +0530

    fifth

---
+++ lib/widgets/meeting_option.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
+
+class MeetingOption extends StatelessWidget {
+  final String text;
+  final bool isMute;
+  final Function(bool) onChange;
+
+  const MeetingOption({
+    Key? key,
+    required this.text,
+    required this.isMute,
+    required this.onChange,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
